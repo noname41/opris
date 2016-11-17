4b956f59d4049b510d3a0e983fe66de4e7c52e08
noname41 <infernal_34@mail.ru>
2016-11-17 18:16:16 +0500

    g

---
+++ WebAppAspnet1/WebAppAspnet/WebAppAspnet/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppAspnet.Startup))]
+namespace WebAppAspnet
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
