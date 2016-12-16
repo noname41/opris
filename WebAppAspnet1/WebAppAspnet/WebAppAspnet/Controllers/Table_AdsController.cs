d9dc2d1a415704e6924ca28e855f777b530131d4
dimitrynekrasov <dinekrasov3@gmail.com>
2016-12-16 15:20:47 +0500

    eto moy commit

---
+++ WebAppAspnet1/WebAppAspnet/WebAppAspnet/Controllers/Table_AdsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppAspnet.Models;
+
+namespace WebAppAspnet.Controllers
+{
+    public class Table_AdsController : Controller
+    {
+        private OPRISEntities12 db = new OPRISEntities12();
+
+        // GET: Table_Ads
+        public ActionResult Index()
+        {
+            return View(db.Table_Ads.ToList());
+        }
+
+        // GET: Table_Ads/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_Ads table_Ads = db.Table_Ads.Find(id);
+            if (table_Ads == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_Ads);
+        }
+
+        // GET: Table_Ads/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Table_Ads/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_Ad,adTitle,adConfirm,adDatePublic,adText,id_Rubric")] Table_Ads table_Ads)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Table_Ads.Add(table_Ads);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table_Ads);
+        }
+
+        // GET: Table_Ads/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_Ads table_Ads = db.Table_Ads.Find(id);
+            if (table_Ads == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_Ads);
+        }
+
+        // POST: Table_Ads/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_Ad,adTitle,adConfirm,adDatePublic,adText,id_Rubric")] Table_Ads table_Ads)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table_Ads).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table_Ads);
+        }
+
+        // GET: Table_Ads/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_Ads table_Ads = db.Table_Ads.Find(id);
+            if (table_Ads == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_Ads);
+        }
+
+        // POST: Table_Ads/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table_Ads table_Ads = db.Table_Ads.Find(id);
+            db.Table_Ads.Remove(table_Ads);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WebAppAspnet1/WebAppAspnet/WebAppAspnet/Controllers/Table_RubricsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebAppAspnet.Models;
+
+namespace WebAppAspnet.Controllers
+{
+    public class Table_RubricsController : Controller
+    {
+        private OPRISEntities12 db = new OPRISEntities12();
+
+        // GET: Table_Rubrics
+        public ActionResult Index()
+        {
+            return View(db.Table_Rubrics.ToList());
+        }
+
+        // GET: Table_Rubrics/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_Rubrics table_Rubrics = db.Table_Rubrics.Find(id);
+            if (table_Rubrics == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_Rubrics);
+        }
+
+        // GET: Table_Rubrics/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Table_Rubrics/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id_Rubric,rubName")] Table_Rubrics table_Rubrics)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Table_Rubrics.Add(table_Rubrics);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(table_Rubrics);
+        }
+
+        // GET: Table_Rubrics/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_Rubrics table_Rubrics = db.Table_Rubrics.Find(id);
+            if (table_Rubrics == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_Rubrics);
+        }
+
+        // POST: Table_Rubrics/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id_Rubric,rubName")] Table_Rubrics table_Rubrics)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(table_Rubrics).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(table_Rubrics);
+        }
+
+        // GET: Table_Rubrics/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Table_Rubrics table_Rubrics = db.Table_Rubrics.Find(id);
+            if (table_Rubrics == null)
+            {
+                return HttpNotFound();
+            }
+            return View(table_Rubrics);
+        }
+
+        // POST: Table_Rubrics/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Table_Rubrics table_Rubrics = db.Table_Rubrics.Find(id);
+            db.Table_Rubrics.Remove(table_Rubrics);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
